da351caed957342b5ff4f1f51dfffd75b9a707b0
Lucas Wang <58446606+lucaswewa@users.noreply.github.com>
2026-02-20 12:56:29 -0800

    add SimulatedProjector
    * v3 checkin
    
    * add  Camera protocol
    
    * add basic server with config and logging
    
    * add SimulatedCamera and SimulatedStage
    
    * add SimulatedProjector
    
    * remove image generation from simulation camera

--- src/gradient_server/wots/projector/simulation_projector.py
+++ src/gradient_server/wots/projector/simulation_projector.py
@@ -1,7 +1,4 @@
-"""Gradient Simulated Camera.
-
-This module defines a Thing that is responsible for using the stage and
-camera together to perform an autofocus routine.
+"""Gradient Simulated Projector.
 
 See repository root for licensing information.
 """
@@ -22,17 +19,12 @@ from PIL import Image, ImageFilter
 import labthings_fastapi as lt
 from labthings_fastapi.types.numpy import NDArray
 
-# from ..stage import SimulatedStage
 from .base_projector import BaseProjector
 
 LOGGER = logging.getLogger(__name__)
 
-# The ratio between "motor" steps and pixels in (x, y, z)
-# higher related to a faster movement
-RATIO = (2, 2, 0.07)
-
 # Some colour variation, for bg detect.
-BG_COLOR = [220, 215, 217]
+BG_COLOR = [0, 0, 0]
 
 # Random Number Generator
 RNG = np.random.default_rng()
@@ -107,22 +99,7 @@ def colour_str_to_colour(colour_str: str) -> tuple[int, int, int]:
 
 
 class SimulatedProjector(BaseProjector):
-    """A Thing that simulates a camera for testing."""
-
-    _show_sample: bool = True
-
-    _objective: int = 40  # default 40x, our standard build
-
-    @lt.property
-    def objective(self) -> int:
-        """Objective magnification (e.g. 4, 10, 20, 40, 60, 100)."""
-        return self._objective
-
-    @objective.setter
-    def _set_objective(self, value: int) -> None:
-        if value not in (4, 10, 20, 40, 60, 100):
-            raise ValueError("Objective must be one of 4, 10, 20, 40, 60, 100.")
-        self._objective = value
+    """A simulated projector for testing."""
 
     def __init__(
         self,
@@ -132,7 +109,7 @@ class SimulatedProjector(BaseProjector):
         canvas_shape: tuple[int, int, int] = (1500, 2000, 3),
         frame_interval: float = 0.1,
     ) -> None:
-        """Initialise the simulated with settings for how images are generated.
+        """Initialise the simulated projector with settings for how images are generated.
 
         :param shape: The shape (size) of the generated image.
         :param canvas_shape: The shape (size) of the canvas generated on initialisation
@@ -151,10 +128,13 @@ class SimulatedProjector(BaseProjector):
         self._capture_thread: Optional[Thread] = None
         self._capture_enabled = False
         self.generate_sprites()
+        self.output_canvas = None
+        self.output_canvas_low_mag = None
+        self.output_blank_canvas = None
+        self._dark_canvas = np.zeros(self.canvas_shape, dtype=np.int16)
+        self._brightness = 1.0
         # Whether the LED is on
         self.led_on = True
-
-    repeating: bool = lt.property(default=False)
 
     _blob_density: int = 400
 
@@ -168,6 +148,17 @@ class SimulatedProjector(BaseProjector):
         self._blob_density = value
         if self._capture_enabled:
             self.generate_canvas()
+            self.generate_output_canvas()
+
+    @lt.property
+    def blob_brightness(self) -> float:
+        """The brightness of blobs."""
+        return self._brightness
+
+    @blob_brightness.setter
+    def _set_blob_brightness(self, value: float) -> None:
+        self._brightness = value
+        self.generate_output_canvas()
 
     # _colour: str = "#b937b9"
 
@@ -190,13 +181,6 @@ class SimulatedProjector(BaseProjector):
         self._color = colour_value
         if self._capture_enabled:
             self.generate_canvas()
-
-    @lt.property
-    def calibration_required(self) -> bool:
-        """Whether the camera needs calibrating."""
-        if self.background_detector is None:
-            return True
-        return not self.background_detector.ready
 
     def generate_sprites(self) -> None:
         """Generate sprites to populate the image."""
@@ -240,6 +224,12 @@ class SimulatedProjector(BaseProjector):
             sprite[:, -1] = 0
             self.sprites.append(sprite)
 
+    def generate_output_canvas(self):
+        self.output_canvas = self.canvas*self._brightness
+        self.output_canvas = self.output_canvas.astype(np.int16)
+        self.output_canvas_low_mag = self.canvas_low_mag*self._brightness
+        self.output_canvas_low_mag = self.output_canvas_low_mag.astype(np.int16)
+        
     def generate_blobs(self, n_blobs: int = 1000) -> None:
         """Generate coordinates of blobs and their sizes, centered around (0,0).
 
@@ -255,6 +245,18 @@ class SimulatedProjector(BaseProjector):
         self.blobs[:, 0] = RNG.uniform(w // 2, self.canvas_shape[1] - w // 2, n_blobs)
         self.blobs[:, 1] = RNG.uniform(w // 2, self.canvas_shape[0] - w // 2, n_blobs)
         self.blobs[:, 2] = RNG.choice(len(self.sprites), n_blobs)
+
+    def get_output_canvas(self):
+        if self.led_on:
+            return self.output_canvas
+        else:
+            return self._dark_canvas
+        
+    def get_output_canvas_low_mag(self):
+        if self.led_on:
+            return self.output_canvas_low_mag
+        else:
+            return self._dark_canvas
 
     def generate_canvas(self) -> None:
         """Generate a canvas with generated blobs centered at the middle.
@@ -307,9 +309,9 @@ class SimulatedProjector(BaseProjector):
 
         sprite_f = sprite.astype(float) / 255
         r, g, b = colour_str_to_colour(self.colour)
-        sprite_r = (255 - r) * sprite_f
-        sprite_g = (255 - g) * sprite_f
-        sprite_b = (255 - b) * sprite_f
+        sprite_r = r * sprite_f
+        sprite_g = g * sprite_f
+        sprite_b = b * sprite_f
         sprite_rgb = np.stack([sprite_r, sprite_g, sprite_b], axis=2)
 
         # Canvas region containing the sprite
@@ -318,94 +320,19 @@ class SimulatedProjector(BaseProjector):
         bottom = min(centre_y + (sprite_h - sprite_h // 2), canvas_h)
         right = min(centre_x + (sprite_w - sprite_w // 2), canvas_w)
 
-        canvas[top:bottom, left:right] -= sprite_rgb.astype("int16")
-
-    def generate_image(self, pos: tuple[int, int, int]) -> Image.Image:
-        """Generate an image with blobs based on supplied coordinates.
-
-        :param pos: a 3-item tuple containing the x,y,z coordinates of the 'stage'
-        """
-        if not self.led_on:
-            return Image.new(mode="RGB", size=(self.shape[1], self.shape[0]), color=0)
-
-        canvas_width, canvas_height, _ = self.low_mag_canvas_shape
-        # Base image size
-
-        objective_downsample = self.objective / 40
-        if objective_downsample >= 0.4:
-            canvas = self.canvas if self._show_sample else self.blank_canvas
-            canvas_width, canvas_height, _ = self.canvas_shape
-            canvas_ds = DOWNSAMPLE
-            img_downsample = DOWNSAMPLE * objective_downsample
-        else:
-            canvas = (
-                self.canvas_low_mag if self._show_sample else self.blank_canvas_low_mag
-            )
-            canvas_width, canvas_height, _ = self.low_mag_canvas_shape
-            canvas_ds = LOW_MAG_DOWNSAMPLE
-            img_downsample = LOW_MAG_DOWNSAMPLE * objective_downsample
-        image_width, image_height, _ = _downsample_shape(self.shape, img_downsample)
-
-        im_pos = (
-            pos[0] * RATIO[0] / canvas_ds,
-            pos[1] * RATIO[1] / canvas_ds,
-            pos[2] * RATIO[2],
-        )
-
-        top_left = (
-            int(im_pos[0]) - image_width // 2 + canvas_width // 2,
-            int(im_pos[1]) - image_height // 2 + canvas_height // 2,
-        )
-
-        x_indices = np.arange(top_left[0], top_left[0] + image_width)
-        y_indices = np.arange(top_left[1], top_left[1] + image_height)
-
-        if self.repeating:
-            # Create index list with modulo rather than slicing to handle wrapping at the
-            # canvas edge.
-            x_indices = x_indices % canvas_width
-            y_indices = y_indices % canvas_height
-        else:
-            # Rather than use a modulo for the index list, as above when wrapping,
-            # this uses np.clip to coerce all out of bound indices to repeat the
-            # first or last pixel in the canvas. This works because no sprite touches
-            # the very edge of the canvas (to prevent partial sprites).
-            x_indices = np.clip(x_indices, 0, canvas_width - 1)
-            y_indices = np.clip(y_indices, 0, canvas_height - 1)
-
-        z_indices = np.arange(self.shape[2])
-
-        # Use npx to make each 1d index list 3D
-        focused_np_img = canvas[np.ix_(x_indices, y_indices, z_indices)]
-        np_img = fast_resize_and_blur(
-            focused_np_img, sigma=np.abs(im_pos[2]) / 5, shape=self.shape
-        )
-        # Generate random noise by repeating 500 noise points, as the speed rather
-        # than randomness is important for simulation.
-        noise = RNG.normal(scale=self.noise_level, size=500).astype("int16")
-        np_img += np.resize(noise, np_img.shape)
-        # Clip then convert to uint8
-        np.clip(np_img, 0, 255, out=np_img)
-        return Image.fromarray(np_img.astype("uint8"))
+        canvas[top:bottom, left:right] += sprite_rgb.astype("int16")
 
     @lt.action
     def set_led(self, led_on: bool = True) -> None:
         """Set the simulated LED to on or off."""
         self.led_on = led_on
 
-    def generate_frame(self) -> Image.Image:
-        """Generate a frame with blobs based on the stage coordinates."""
-        # Simulate LED turning off by setting all channels to 0
-        if not self.led_on:
-            return Image.new(mode="RGB", size=(self.shape[1], self.shape[0]), color=0)
-        # Otherwise, generate a frame from current position
-        return self.generate_image((0, 0, 0))
-
     def __enter__(self) -> Self:
         """Start the capture thread when the Thing context manager is opened."""
         super().__enter__()
         self.generate_canvas()
-        self.start_streaming()
+        self.generate_output_canvas()
+
         return self
 
     def __exit__(
@@ -419,138 +346,6 @@ class SimulatedProjector(BaseProjector):
             self._capture_enabled = False
             self._capture_thread.join()
         super().__exit__(exc_type, exc_value, traceback)
-
-    @lt.action
-    def start_streaming(
-        self, main_resolution: tuple[int, int] = (820, 616), buffer_count: int = 1
-    ) -> None:
-        """Start the live stream.
-
-        The start_streaming method is used a camera ``Thing`` to begin streaming
-        images or to adjust the stream resolution if streaming is already active.
-
-        The simulation camera does not currently support the resolution argument.
-        It will always issue a warning that the resolution is not respected.
-        If called while already streaming, the warning will be emitted and no other
-        action will be taken.
-
-        :param main_resolution: Currently ignored, this argument exists to ensure consistent API across camera Things.
-        :param buffer_count: Currently ignored, this argument exists to ensure consistent API across camera Things.
-        """
-        LOGGER.warning(
-            f"Simulation camera doesn't respect {main_resolution=} or {buffer_count=} "
-            "arguments."
-        )
-        if not self.stream_active:
-            self._capture_enabled = True
-            self._capture_thread = Thread(target=self._capture_frames)
-            self._capture_thread.start()
-
-    @lt.property
-    def stream_active(self) -> bool:
-        """Whether the MJPEG stream is active."""
-        if self._capture_enabled and self._capture_thread:
-            return self._capture_thread.is_alive()
-        return False
-
-    noise_level: float = lt.property(default=2.0)
-
-    def _capture_frames(self) -> None:
-        last_frame_t = time.time()
-        while self._capture_enabled:
-            wait_time = self.frame_interval - (time.time() - last_frame_t)
-            if wait_time > 0:
-                time.sleep(wait_time)
-            last_frame_t = time.time()
-
-            frame = self.generate_frame()
-            self.mjpeg_stream.add_frame(_frame2bytes(frame))
-            ds_frame = frame.resize((320, 240), resample=Image.Resampling.NEAREST)
-            self.lores_mjpeg_stream.add_frame(_frame2bytes(ds_frame))
-
-    @lt.action
-    def discard_frames(self) -> None:
-        """Discard frames so that the next frame captured is fresh.
-
-        There is nothing to do as this is a simulation!
-        """
-
-    @lt.action
-    def capture_array(
-        self,
-        stream_name: Literal["main", "lores", "raw", "full"] = "full",
-        wait: Optional[float] = None,
-    ) -> NDArray:
-        """Acquire one image from the camera and return as an array.
-
-        This function will produce a nested list containing an uncompressed RGB image.
-        It's likely to be highly inefficient - raw and/or uncompressed captures using
-        binary image formats will be added in due course.
-
-        :param stream_name: Currently ignored, this argument exists to ensure consistent API across camera Things.
-        :param wait: Currently ignored, this argument exists to ensure consistent API across camera Things.
-        """
-        if wait is not None:
-            LOGGER.warning("Simulation camera has no wait option. Use None.")
-        LOGGER.warning(f"Simulation camera camera doesn't respect {stream_name=}")
-        return np.array(self.generate_frame())
-
-    def capture_image(
-        self,
-        stream_name: Literal["main", "lores", "full"],
-        wait: Optional[float] = None,
-    ) -> Image.Image:
-        """Capture to a PIL image. This is not exposed as a ThingAction.
-
-        It is used for capture to memory.
-
-        :param stream_name: Currently ignored, this argument exists to ensure consistent API across camera Things.
-        :param wait: Currently ignored, this argument exists to ensure consistent API across camera Things.
-        """
-        if wait is not None:
-            LOGGER.warning("Simulation camera has no wait option. Use None.")
-        LOGGER.warning(f"Simulation camera camera doesn't respect {stream_name=}")
-        return self.generate_frame()
-
-    @lt.action
-    def full_auto_calibrate(self) -> None:
-        """Perform a full auto-calibration.
-
-        For the simulation microscope the process is:
-
-        * ``remove_sample``
-        * ``set_background``
-        * ``load_sample``
-        """
-        self.remove_sample()
-        time.sleep(0.2)
-        if self.background_detector is not None:
-            self.set_background()
-        time.sleep(0.2)
-        self.load_sample()
-
-    @lt.action
-    def remove_sample(self) -> None:
-        """Show the simulated background with no sample."""
-        if not self._show_sample:
-            raise RuntimeError("Sample is already removed.")
-        self._show_sample = False
-
-    @lt.action
-    def load_sample(self) -> None:
-        """Show the simulated sample."""
-        if self._show_sample:
-            raise RuntimeError("Sample is already in place.")
-        self._show_sample = True
-
-
-def _frame2bytes(frame: Image.Image) -> bytes:
-    """Convert frame to bytes."""
-    with io.BytesIO() as buf:
-        # Save in low quality for speed.
-        frame.save(buf, format="JPEG", quality=85)
-        return buf.getvalue()
-
 
 def fast_resize_and_blur(
     array: np.ndarray, sigma: float, shape: tuple[int, ...]
