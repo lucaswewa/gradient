9d9d0461e7523d1c556e2487ee3d9f52f7369169
Lucas Wang <iwewa@hotmail.com>
2026-03-25 12:29:31 -0700

    conex

--- src/gradient_server/wots/__init__.py
+++ src/gradient_server/wots/__init__.py
@@ -3,9 +3,23 @@
 import logging
 
 import labthings_fastapi as lt
+import anyio
+import time
+from anyio.from_thread import BlockingPortal
+import random
 
 LOGGER = logging.getLogger(__name__)
 
+async def async_method(t):
+    print("async method start")
+    await anyio.sleep(t)
+    print("async method done!")
+
+def sync_method(t, r, portal: BlockingPortal):
+    print(f"Sync method: start {r}")
+    time.sleep(t)
+    portal.start_task_soon(async_method, t)
+    print(f"sync method: done! {r}")
 
 class MyThing(lt.Thing):
     """A test WoT thing."""
@@ -15,3 +29,7 @@ class MyThing(lt.Thing):
         """Get AI."""
         LOGGER.info("get ai")
         return "ai"
+
+    @lt.action
+    def ac(self, t: float, portal: lt.deps.BlockingPortal) -> None:
+        sync_method(t, random.random(), portal)
