2dc9fbe978c37896958cff56b4b384198e0e2c47
Danny Wang <danny.wang@tetaprecision.com>
2026-03-27 15:56:13 -0700

    subscribe for status update

--- src/gradient_server/wots/__init__.py
+++ src/gradient_server/wots/__init__.py
@@ -25,6 +25,33 @@ def sync_method(t, r, portal: BlockingPortal):
     portal.call(async_method, t)
     print(f"sync method: done! {r}")
 
+class CircularBuffer:
+    def __init__(self, capacity):
+        self.capacity = capacity
+        self.buffer = [None] * capacity
+        self.head = 0  # Index to the next write position
+        self.count = 0 # Current number of elements in the buffer
+
+    def append(self, value):
+        self.buffer[self.head] = value
+        self.head = (self.head + 1) % self.capacity
+        if self.count < self.capacity:
+            self.count += 1
+
+    def get(self):
+        # Return elements in order from oldest to newest
+        if self.count == 0:
+            return []
+        
+        # Calculate the starting index of the oldest element
+        start_index = (self.head - self.count + self.capacity) % self.capacity
+        
+        # If the buffer has wrapped around, concatenate two slices
+        if start_index + self.count <= self.capacity:
+            return self.buffer[start_index : start_index + self.count]
+        else:
+            return self.buffer[start_index:] + self.buffer[:self.head]
+
 class MyThing(lt.Thing):
     """A test WoT thing."""
 
@@ -33,6 +60,10 @@ class MyThing(lt.Thing):
         self.tg = None
         self.running = False
         self._cmd_id = 0
+        self._temp1 = 0.0
+        self._temp2 = 0.0
+        self._temp_history1 = CircularBuffer(100)
+        self._temp_history2 = CircularBuffer(100)
 
     async def __aenter__(self):
         await self.start()        
@@ -101,11 +132,16 @@ class MyThing(lt.Thing):
                 message = await self.ws.recv()
                 obj = json.loads(message)
                 if "method" in obj.keys():
-                    print(obj["method"])
                     if obj["method"] == "notify_status_update":
-                        print("yeah")
-                        print(obj["params"][0]["motion_report"]["live_position"])
-                        print(obj["params"][1])
+                        if "motion_report" in obj["params"][0].keys() and "live_position" in obj["params"][0]["motion_report"].keys():
+                            print("live_position:", obj["params"][0]["motion_report"]["live_position"])
+                        if "heater_bed" in obj["params"][0].keys() and "temperature" in obj["params"][0]["heater_bed"].keys():
+                            self._temp1 = obj["params"][0]["heater_bed"]["temperature"]
+                            self._temp_history1.append(self._temp1)
+                        if "extruder" in obj["params"][0].keys() and "temperature" in obj["params"][0]["extruder"].keys():
+                            self._temp2 = obj["params"][0]["extruder"]["temperature"]
+                            self._temp_history2.append(self._temp2)
+
                 await self.tx.send(obj)
             except websockets.exceptions.ConnectionClosed as e:
                 print(f"Connection closed: {e}")
@@ -120,6 +156,22 @@ class MyThing(lt.Thing):
                 raise
             
     
+    @lt.property
+    def temp1(self) -> float:
+        return self._temp1
+    
+    @lt.property
+    def temp2(self) -> float:
+        return self._temp2
+    
+    @lt.property
+    def temp1_history(self) -> list[float]:
+        return self._temp_history1.get()
+    
+    @lt.property
+    def temp2_history(self) -> list[float]:
+        return self._temp_history2.get()
+
     @lt.property
     def ai(self) -> str:
         """Get AI."""
@@ -147,6 +199,14 @@ class MyThing(lt.Thing):
         task, status = portal.start_task(self.send_cmd, self.ws, self.rx, conn)
         print(task)
         return task._result
+
+    @lt.action
+    def begin_session(self, portal: lt.deps.BlockingPortal) -> Any:
+        sub = {"jsonrpc":"2.0","method":"printer.objects.subscribe","params":{"objects":{"gcode":None,"webhooks":None,"configfile":None,"mcu":None,"stepper_enable":None,"tmc2209 stepper_x":None,"tmc2209 stepper_y":None,"tmc2209 stepper_z":None,"tmc2209 stepper_z1":None,"tmc2209 stepper_z2":None,"tmc2209 extruder":None,"heaters":None,"heater_bed":None,"probe":None,"gcode_move":None,"bed_mesh":None,"fan":None,"heater_fan hotend_fan":None,"controller_fan controller_fan":None,"idle_timeout":None,"z_tilt":None,"display_status":None,"gcode_macro PRINT_START":None,"gcode_macro PRINT_END":None,"print_stats":None,"virtual_sdcard":None,"pause_resume":None,"gcode_macro CANCEL_PRINT":None,"gcode_macro PAUSE":None,"gcode_macro RESUME":None,"gcode_macro SET_PAUSE_NEXT_LAYER":None,"gcode_macro SET_PAUSE_AT_LAYER":None,"gcode_macro SET_PRINT_STATS_INFO":None,"gcode_macro _TOOLHEAD_PARK_PAUSE_CANCEL":None,"gcode_macro _CLIENT_EXTRUDE":None,"gcode_macro _CLIENT_RETRACT":None,"gcode_macro _CLIENT_LINEAR_MOVE":None,"query_endstops":None,"motion_report":None,"toolhead":None,"extruder":None,"system_stats":None,"manual_probe":None}},"id":30}
+        task, status = portal.start_task(self.send_cmd, self.ws, self.rx, sub)
+        print(task)
+        return task._result
+
 
     @lt.action
     def info(self, portal: lt.deps.BlockingPortal) -> Any:
@@ -183,5 +243,6 @@ class MyThing(lt.Thing):
             cmd2 = {"jsonrpc":"2.0","method":"printer.gcode.script","params":{"script":f"_CLIENT_LINEAR_MOVE Z={z_pos_end} F=1500 ABSOLUTE=1"},"id":34}
             task, result=portal.start_task(self.send_cmd, self.ws, self.rx, cmd2)
             time.sleep(0.25)
-            return task._result
+
+        return task._result
 
